d366be54e001abc757af927a440140af14400dd6
ayomide-shinaaluko <shinaalukoayomide@yahoo.co.uk>
2023-09-20 23:10:48 +0100

    imported industry route

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const dbConnect = require("./config/dbConnect");
 const cors = require("cors");
 const deviceRoutes = require("./routes/deviceRoutes");
+const industryRoutes = require("./routes/industryRoutes");
 
 // express app
 const app = express();
@@ -15,6 +16,7 @@ app.use(cors());
 
 // routes
 app.use("/api/devices", deviceRoutes);
+app.use("/api/industries", industryRoutes);
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
