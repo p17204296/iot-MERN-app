4b1599560f2ca47ad03488cbdd4f8fb175977669
ayomide-shinaaluko <shinaalukoayomide@yahoo.co.uk>
2023-09-21 22:48:40 +0100

    tests for get requests (all & by id)

--- server/spec/deviceController.spec.js
+++ server/spec/deviceController.spec.js
@@ -25,6 +25,74 @@ describe("Device Controller Unit Tests", () => {
     await deviceModel.deleteMany({});
   });
 
+  it("should get all devices", async () => {
+    // Create a new industry to link with the device
+    const industry = await industryModel.create({
+      industryName: "Test Industry",
+    });
+    // Create some test devices in the database
+    const device1 = await deviceModel.create({
+      deviceName: "Device 1",
+      fee: 100,
+      linkedIndustry: industry.industryName,
+    });
+    const device2 = await deviceModel.create({
+      deviceName: "Device 2",
+      fee: 200,
+      linkedIndustry: "Test Industry",
+    });
+    const device3 = await deviceModel.create({
+      deviceName: "Device 3",
+      fee: 300,
+      linkedIndustry: "Test Industry",
+    });
+
+    // Make a GET request to retrieve all devices
+    const response = await request(app).get("/api/devices");
+
+    // Verify the response status code and the number of devices returned
+    expect(response.status).toBe(200);
+    expect(Array.isArray(response.body.devices)).toBe(true);
+    expect(response.body.devices.length).toBe(3);
+
+    // Verify the content of the first device in the response
+    expect(response.body.devices[0].deviceName).toBe("Device 1");
+    expect(response.body.devices[0].fee).toBe(100);
+  });
+
+  it("should get a single device by ID", async () => {
+    // Create a new industry to link with the device
+    const industry = await industryModel.create({
+      industryName: "Test Industry",
+    });
+    // Create a new device in the database
+    const newDevice = await deviceModel.create({
+      deviceName: "New Device",
+      fee: 400,
+      linkedIndustry: industry.industryName,
+    });
+
+    // Make a GET request to retrieve the device by its ID
+    const response = await request(app).get(`/api/devices/${newDevice._id}`);
+
+    // Verify the response status code and the device data
+    expect(response.status).toBe(200);
+    expect(response.body.deviceName).toBe("New Device");
+    expect(response.body.fee).toBe(400);
+  });
+
+  it("should return a 404 error when getting a non-existent device by ID", async () => {
+    // Attempt to retrieve a non-existent device by ID
+    const nonExistentDeviceId = mongoose.Types.ObjectId();
+    const response = await request(app).get(
+      `/api/devices/${nonExistentDeviceId}`
+    );
+
+    // Verify the response status code and error message
+    expect(response.status).toBe(404);
+    expect(response.body.error).toBe("No such device");
+  });
+
   it("should create a new device", async () => {
     // Create a new industry to link with the device
     const industry = await industryModel.create({
@@ -52,17 +120,21 @@ describe("Device Controller Unit Tests", () => {
   });
 
   it("should update an existing device", async () => {
+    const industry = await industryModel.create({
+      industryName: "Test Industry",
+    });
+
     // Create an existing device in the database
     const existingDevice = await deviceModel.create({
       deviceName: "Existing Device",
       fee: 200,
-      linkedIndustry: "Test Industry",
+      linkedIndustry: industry.industryName,
     });
 
     // Update data for the existing device
     const updatedData = {
-      deviceName: "Updated Device!!",
-      fee: 1510,
+      deviceName: "Updated Device",
+      fee: 150,
     };
 
     // Make a PATCH request to update the existing device
