ac808677853ba5caafbacaa7280200df2e689209
ayomide-shinaaluko <shinaalukoayomide@yahoo.co.uk>
2023-09-25 00:13:28 +0100

    page navigation

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,45 @@
 import "./App.css";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Devices from "./pages/Devices/Devices";
 
+import Industries from "./pages/Industries/Industries";
+
 function App() {
+  const [openDevices, setDevices] = useState(true);
+  const [openIndustry, setIndustries] = useState(false);
+
+  function showDevices() {
+    setDevices(true);
+    setIndustries(false);
+  }
+
+  function showIndustries() {
+    setIndustries(true);
+    setDevices(false);
+  }
+
   return (
     <div className="App">
       <h1>IoT App - MERN STACK</h1>
-      <Devices />
+      {/* <Devices /> */}
+
+      {openDevices & !openIndustry ? <Devices /> : <Industries />}
+      <div className="view-selection-div">
+        <button
+          className="view-button active-button"
+          onClick={() => showDevices()}
+        >
+          Devices Page
+        </button>
+        <button
+          data-testid="openIndustryTab"
+          className="view-button active-button"
+          onClick={() => showIndustries()}
+        >
+          Industry Page
+        </button>
+      </div>
     </div>
   );
 }
