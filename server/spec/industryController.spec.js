ad9de97ae545391d788b4e9436e494dfda2973c8
ayomide-shinaaluko <shinaalukoayomide@yahoo.co.uk>
2023-09-25 12:09:51 +0100

    Increase Industry Test Coverage

--- server/spec/industryController.spec.js
+++ server/spec/industryController.spec.js
@@ -3,6 +3,10 @@ const app = require("../server");
 const industryModel = require("../models/industryModel");
 const mongoose = require("mongoose");
 
+const HTTP_STATUS_OK = 200;
+const HTTP_STATUS_CREATED = 201;
+const HTTP_STATUS_NOT_FOUND = 404;
+
 beforeAll(async () => {
   console.log("\n ---------- Industry Controller Tests: ---------- \n ");
   await mongoose.disconnect();
@@ -35,7 +39,7 @@ describe("Industry Controller Unit Tests", () => {
       .send(industryData);
 
     // Verify the response status code and structure
-    expect(response.status).toBe(201);
+    expect(response.status).toBe(HTTP_STATUS_CREATED);
     expect(response.body.industryName).toBe("Test Industry");
 
     // Ensure the created industry exists in the database
@@ -86,5 +90,69 @@ describe("Industry Controller Unit Tests", () => {
     // Ensure the industry is deleted from the database
     const deletedIndustry = await industryModel.findById(existingIndustry._id);
     expect(deletedIndustry).toBeNull();
+  });
+
+  it("should get a single industry by ID", async () => {
+    // Create a test industry
+    const industryData = {
+      industryName: "Test Industry",
+    };
+    const createdIndustry = await industryModel.create(industryData);
+
+    // Make a GET request to retrieve the industry by its ID
+    const response = await request(app).get(
+      `/api/industries/${createdIndustry._id}`
+    );
+
+    // Verify the response status code and industry data
+    expect(response.status).toBe(HTTP_STATUS_OK);
+    expect(response.body.industryName).toBe("Test Industry");
+  });
+
+  it("should return a 404 error when getting a non-existent industry by ID", async () => {
+    // Attempt to retrieve a non-existent industry by ID
+    const nonExistentIndustryId = mongoose.Types.ObjectId();
+    const response = await request(app).get(
+      `/api/industries/${nonExistentIndustryId}`
+    );
+
+    // Verify the response status code and error message
+    expect(response.status).toBe(HTTP_STATUS_NOT_FOUND);
+    expect(response.body.error).toBe("No such industry");
+  });
+
+  it("should get all industries sorted by industryName in ascending order", async () => {
+    // Create multiple test industries with different names
+    await industryModel.create({ industryName: "Zebra Industry" });
+    await industryModel.create({ industryName: "Apple Industry" });
+
+    // Make a GET request to retrieve all industries sorted by industryName in ascending order
+    const response = await request(app)
+      .get("/api/industries")
+      .query({ sort: "industryName,asc" });
+
+    // Verify the response status code and industry order
+    expect(response.status).toBe(HTTP_STATUS_OK);
+    const industries = response.body.industries;
+    expect(industries[0].industryName).toBe("Apple Industry");
+    expect(industries[1].industryName).toBe("Zebra Industry");
+  });
+
+  it("should get industries with pagination (page and limit)", async () => {
+    // Create multiple test industries
+    await industryModel.create({ industryName: "Industry 1" });
+    await industryModel.create({ industryName: "Industry 2" });
+    await industryModel.create({ industryName: "Industry 3" });
+
+    // Make a GET request to retrieve industries with pagination
+    const response = await request(app)
+      .get("/api/industries")
+      .query({ page: 2, limit: 1 });
+
+    // Verify the response status code and industry data
+    expect(response.status).toBe(HTTP_STATUS_OK);
+    const industries = response.body.industries;
+    expect(industries.length).toBe(1);
+    expect(industries[0].industryName).toBe("Industry 2");
   });
 });
